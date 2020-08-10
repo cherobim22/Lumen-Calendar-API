895ffe83d2273a3fb2e52a47ee2476fed1bd29ad
Lucas Cherobim <lucascherobim@hotmail.com>
2020-08-10 18:15:35 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,19 @@
 |--------------------------------------------------------------------------
 |
 | Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
+| It is a breeze. Simply tell Lumen the URIs it should respond tocreate
 | and give it the Closure to call when that URI is requested.
 |
 */
 //autenticação
 $router->get('/auth', 'LoginController@Auth');
 $router->get('/callback', 'LoginController@Callback');
+
+//Database
+$router->get('/clients', 'ClientController@buscarTodos');
+$router->get('/clients/{id}', 'ClientController@buscarClientes');
+$router->delete('/clients/{id}', 'ClientController@deletarClientes');
+$router->put('/clients/{id}', 'ClientController@updateClient');
 
 //calendario
 $router->get('/calendars/{id}', 'GoogleClientController@listCalendars');
