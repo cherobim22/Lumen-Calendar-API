204982083c5924ca6fd25a67abf7dd6d8762c0c6
Lucas Cherobim <lucascherobim@hotmail.com>
2020-08-14 22:27:19 -0300

    update login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,7 +7,7 @@ use App\Helpers\GoogleClientHelper;
 use Illuminate\Http\Request;
 
 
-class LoginController extends Controller {
+class GoogleLoginController extends Controller {
     //__contruct
     public function __construct(GoogleClientHelper $clientHelper){
         $clientHelper = new GoogleClientHelper;
@@ -26,13 +26,21 @@ class LoginController extends Controller {
     /**
      * Funcao que retorna e salva o token no banco
      */
-    public function Callback(Request $request){
+    public function Callback (Request $request){
         $this->clientHelper->setCode($request->get('code'));
         $token = $this->clientHelper->getToken();
+
+        $state = $request->get('state');
+        $state_64 = base64_decode($state);
+        $stateDecode = json_decode($state_64, true);
+        $user_id = $stateDecode['user_id'];
+        //decode
+        //
+        print_r(json_encode($user_id));
         print_r(json_encode($token));
         //model
         $this->client = new GoogleClient;
-        $this->client->blog_id = '333';
+        $this->client->user_id = $user_id;
         $this->client->access_token =  $token['access_token'];
         $this->client->expires_in = $token['expires_in'];
         $this->client->refresh_token = $token['refresh_token'];
