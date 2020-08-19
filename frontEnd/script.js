bd1046284b682c6d2aa64d81aae8155237c58349
Lucas Cherobim <lucascherobim@hotmail.com>
2020-08-19 20:51:17 -0300

    update front

---
+++ frontEnd/script.js
@@ -0,0 +1,31 @@
+function abrir(URL) {
+    window.open(URL, 'janela', 'width=795, height=590, top=100, left=699, scrollbars=no, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no')}
+
+function openForm() {
+    document.getElementById("popup_form").style.display = "block";
+    document.getElementById("cancel").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("popup_form").style.display = "none";
+    document.getElementById("cancel").style.display = "none";
+}
+
+function myFunction() {
+    var checkBox = document.getElementById("lead");
+    var text = document.getElementById("email_lead");
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+function addCalendar() {
+    var checkBox = document.getElementById("calendar");
+    var text = document.getElementById("div_form");
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
