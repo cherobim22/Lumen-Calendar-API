63676fe5fa1e62d11ce0fec3f2140b3b068e1604
Lucas Cherobim <lucascherobim@hotmail.com>
2020-08-19 20:54:10 -0300

    update front

--- frontEnd/calendars.php
+++ frontEnd/calendars.php
@@ -1,4 +1,21 @@
 <?php
 $user = $_REQUEST['user'];
+
 print_r($user);
+
+$iniciar = curl_init();
+curl_setopt($iniciar, CURLOPT_URL, "http://lucas.innovaweb.com.br/google/events/create/");
+curl_setopt($iniciar, CURLOPT_POST, true);
+curl_setopt($iniciar, CURLOPT_HTTPHEADER, array(
+    'Authorization: '
+));
+curl_setopt($iniciar, CURLOPT_POSTFIELDS, $user);
+curl_setopt($iniciar, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($iniciar);
+
+curl_close ($iniciar);
+
+return print_r(json_encode($response));
+
 ?>
